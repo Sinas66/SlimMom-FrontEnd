9ab2d01c5e07f4331f3575a17063c7f6b0f67d8b
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-08-07 00:30:59 +0300

    first commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,77 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
-  state = {};
+  state = {
+    redirectToPreviousRoute: false,
+    nickname: '',
+    password: ''
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { nickname, password } = this.state;
+
+    this.props.logIn(
+      {
+        nickname,
+        password
+      },
+      () => {
+        this.setState({ redirectToPreviousRoute: true });
+      }
+    );
+  };
+
+  handleChange = e => {
+    const value = e.currentTarget.value;
+    const fieldName = e.currentTarget.dataset.fieldName;
+
+    this.setState(prev => ({
+      ...prev,
+      [fieldName]: value
+    }));
+  };
 
   render() {
-    return <p>Login</p>;
+    const { location, errorMsg } = this.propsconst;
+    const { from } = location.state || { from: { pathname: '/' } };
+    const { nickname, password, redirectToPreviousRoute } = this.state;
+
+    if (redirectToPreviousRoute) {
+      return <Redirect to={form} />;
+    }
+
+    return (
+      <div>
+        {errorMsg && <p>{errorMsg}</p>}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            data-field-name={'nickname'}
+            type={'text'}
+            onChange={this.handleChange}
+            placeholder={'Nickname'}
+            value={nickname}
+          />
+          <input
+            data-field-name={'password'}
+            type={'text'}
+            onChange={this.handleChange}
+            placeholder={'Password'}
+            value={password}
+          />
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  logIn: PropTypes.func.isRequired,
+  errorMsg: PropTypes.string,
+  nickname: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+};
 
 export default Login;
