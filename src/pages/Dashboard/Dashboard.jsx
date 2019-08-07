3b42bd18209a7964d3ff949d439909b999da2d5f
YarotskiY <abrawa@gmail.com>
2019-08-07 16:57:15 +0300

    create structure of componnet

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Dashboard.module.css';
+import windowSize from 'react-window-size';
 
-const Dashboard = () => {
-  return (
-    <>
-    <section className={styles.grid}>
-      <div className={styles.header}>Header</div>
-      <div className={styles.calkBlock}>CalcBlock</div>
-      <div className={styles.summary}>Summary</div>
-    </section>
-    </>
-  )
+class Dashboard extends Component {
+  state = {
+    isCalcOpen: true
+  };
+
+  toggleIsCalcOpen = () => {
+    this.setState(state=>({
+      isCalcOpen: !state.isCalcOpen
+    }))
+  }
+
+
+  render() {
+    const { isCalcOpen } = this.state;
+    return <>
+            <section className={styles.grid}>
+             <div className={styles.headerBlock_container}>
+              Header
+               </div>
+             <div className={styles.calcDairyBlock_container}>
+                Calc/DairyBlock
+               </div>
+             <div className={styles.summaryBlock_container}>
+              SummaryBlock
+               </div>
+           </section>
+          </>;
+  }
 }
 
 export default Dashboard;
