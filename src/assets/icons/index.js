8468465bd94371c005464d95f669967ee5c0e5a3
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-08-07 18:06:31 +0300

    icons are connected

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,12 @@
+// import { ReactComponent as Exit } from './exit.svg';
+import { ReactComponent as Arrow_Back } from './arrow-back.png';
+import { ReactComponent as Arrow_Down } from './arrow-down.png';
+import { ReactComponent as Arrow_Up } from './arrow-up.png';
+import { ReactComponent as Calendar } from './calendar.png';
+import { ReactComponent as Close } from './close.png';
+import { ReactComponent as Diary_Logout } from './diary_logout.png';
+import { ReactComponent as Flag_Ru } from './flag_ru.png';
+import { ReactComponent as Flag_Ua } from './flag_ukr.png';
+import { ReactComponent as Menu } from './menu.png';
 
-import { ReactComponent as Exit } from './exit.svg';
-
-
-export {  Exit };
+export { Arrow_Back, Arrow_Down, Arrow_Up, Calendar, Close, Diary_Logout, Flag_Ru, Flag_Ua, Menu };
