68223d33dbd2befe42ca906bdf387d69a3cce99c
katehrybkova <katiushka152701@gmail.com>
2019-08-07 22:41:53 +0300

    fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from './Home.module.css';
-// import CalcForm from '../../components/CalcForm/CalcForm';
-// import Result from '../../components/Result/Result';
-// import Header from '../../components/Header/Header';
-// import UserBar from '../../components/UserBar/UserBar';
 
-class Home extends Component {
-  state = {
-    isOpenModal: false
-  };
-  handleToggleModal = () => {
-    this.setState(state => ({
-      isOpenModal: !state.isOpenModal
-    }));
-  };
-  render() {
-    const { isOpenModal } = this.state;
-    return (
-      <div className={style.homeContainer}>
-        <div className={style.homeGroup}>
-            {/* <Header />
-            <UserBar />
-            <CalcForm />
-          <div>{isOpenModal && <Result onClose={this.handleToggleModal} />}</div> */}
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className={style.homeContainer}>
+    <div className={style.homeGroup}>
+      <div className={style.homeHeader}>header</div>
+      <div>calcform</div>
+    </div>
+  </div>
+);
 
 export default Home;
