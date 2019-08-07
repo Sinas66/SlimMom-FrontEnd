0fa36855d647a349324a26c21f47defd61c8c309
StellVan <balan.marian2000@gmail.com>
2019-08-07 10:33:58 +0300

    header-tablet-finished

--- src/components/Header/Modal.js
+++ src/components/Header/Modal.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 import styles from './Modal.module.css';
-
+import { NavLink } from 'react-router-dom';
 
 const Modal = () => {
-    return (
-        <div className={styles.container}>
-            <a href="#">Diary</a>
-            <a href="#">Calculator</a>
-            {/* <nav><NavLink exact to='/diary'>Дневник</NavLink>
-                <NavLink exact to='/calc'>Калькулятор</NavLink>
+  return (
+    <div className={styles.container}>
+      {/* <a href="#">Diary</a>
+      <a href="#">Calculator</a> */}
+      <div className={styles.linkBox}>
+        <NavLink exact to="/diary">
+          Дневник
+        </NavLink>
+        <NavLink exact to="/calc">
+          Калькулятор
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
-
-            </nav> */}
-        </div>
-    )
-}
-
-export default Modal
+export default Modal;
--- src/components/UserBar/UserBar.js
+++ src/components/UserBar/UserBar.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import styles from './UserBar.module.css';
 export default class UserBar extends Component {
-    render() {
-        return (
-            <div className={styles.container}>
-                <a className={styles.link} href="#">Вход | Регистрация</a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.container}>
+        <a className={styles.link} href="#">
+          Вход <span className={styles.spanLine}>|</span> Регистрация
+        </a>
+      </div>
+    );
+  }
 }
