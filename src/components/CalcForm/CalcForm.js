1937b1dc999db399b9a98519551883097dd99b5c
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-08-07 13:38:54 +0300

    test

--- src/components/CalcForm/CalcForm.js
+++ src/components/CalcForm/CalcForm.js
@@ -32,71 +32,75 @@ class CalcForm extends Component {
           <p className={css.title}>Узнай свою суточную </p>
           <p className={css.title}>норму калорий</p>
           <form>
-            <label htmlFor="height">
-              <input
-                className={css.input}
-                id="height"
-                type="number"
-                placeholder="Рост *"
-                name="height"
-                value={height}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label htmlFor="age">
-              <input
-                className={css.input}
-                id="age"
-                type="number"
-                placeholder="Возраст *"
-                name="age"
-                value={age}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label htmlFor="currentWeight">
-              <input
-                className={css.input}
-                id="currentWeight"
-                type="number"
-                placeholder="Текущий вес *"
-                name="currentWeight"
-                value={currentWeight}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label htmlFor="desireWeight">
-              <input
-                className={css.input}
-                id="desireWeight"
-                type="number"
-                placeholder="Желаемый вес *"
-                name="desireWeight"
-                value={desireWeight}
-                onChange={this.handleChange}
-              />
-            </label>
-            <section className={css.radioContainer}>
-              <h3>Группа крови *</h3>
-              <div className={css.radioInputs}>
-                <label htmlFor="groupBlood_1">
-                  1
-                  <input id="groupBlood_1" type="radio" name="groupBlood" value="1" onChange={this.handleChange} />
-                </label>
-                <label htmlFor="groupBlood_2">
-                  2
-                  <input id="groupBlood_2" type="radio" name="groupBlood" value="2" onChange={this.handleChange} />
-                </label>
-                <label htmlFor="groupBlood_3">
-                  3
-                  <input id="groupBlood_3" type="radio" name="groupBlood" value="3" onChange={this.handleChange} />
-                </label>
-                <label htmlFor="groupBlood_4">
-                  4
-                  <input id="groupBlood_3" type="radio" name="groupBlood" value="4" onChange={this.handleChange} />
-                </label>
-              </div>
-            </section>
+            <div className={css.leftInputs}>
+              <label htmlFor="height">
+                <input
+                  className={css.input}
+                  id="height"
+                  type="number"
+                  placeholder="Рост *"
+                  name="height"
+                  value={height}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="age">
+                <input
+                  className={css.input}
+                  id="age"
+                  type="number"
+                  placeholder="Возраст *"
+                  name="age"
+                  value={age}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="currentWeight">
+                <input
+                  className={css.input}
+                  id="currentWeight"
+                  type="number"
+                  placeholder="Текущий вес *"
+                  name="currentWeight"
+                  value={currentWeight}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <div className={css.rightInputs}>
+              <label htmlFor="desireWeight">
+                <input
+                  className={css.input}
+                  id="desireWeight"
+                  type="number"
+                  placeholder="Желаемый вес *"
+                  name="desireWeight"
+                  value={desireWeight}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <section className={css.radioContainer}>
+                <h3>Группа крови *</h3>
+                <div className={css.radioInputs}>
+                  <label htmlFor="groupBlood_1">
+                    1
+                    <input id="groupBlood_1" type="radio" name="groupBlood" value="1" onChange={this.handleChange} />
+                  </label>
+                  <label htmlFor="groupBlood_2">
+                    2
+                    <input id="groupBlood_2" type="radio" name="groupBlood" value="2" onChange={this.handleChange} />
+                  </label>
+                  <label htmlFor="groupBlood_3">
+                    3
+                    <input id="groupBlood_3" type="radio" name="groupBlood" value="3" onChange={this.handleChange} />
+                  </label>
+                  <label htmlFor="groupBlood_4">
+                    4
+                    <input id="groupBlood_3" type="radio" name="groupBlood" value="4" onChange={this.handleChange} />
+                  </label>
+                </div>
+              </section>
+            </div>
           </form>
         </div>
       </div>
