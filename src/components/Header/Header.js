baccc5e17673bef61e782e80c421e1f22e944520
Valerii Kovalenko <kovalenko.vv.smartagency@gmail.com>
2019-08-07 16:29:13 +0300

    Loginization

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,64 +1,98 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
 import UserBar from '../UserBar/UserBar';
+import windowSize from 'react-window-size';
 import logo from './Logo/logo-png.png';
 import burger from './Logo/burger.svg';
 import cross from './Logo/cross.png';
 import logout from './Logo/logout.png';
 import styles from './Header.module.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
-  state = {
-    openModal: false,
-    isLogged: true,
-    toogleIcon: false
-  };
 
-  toogleModal = e => {
-    this.setState(state => ({ openModal: !state.openModal, toogleIcon: !state.toogleIcon }));
-  };
-  // Here I have to write a function than will check if the User is LoggedIn or not, and change the state apropriately
+// const userToken = JSON.parse(localStorage.getItem('userToken'))
 
-  render() {
-    const { toogleModal } = this;
-    const { openModal, isLogged, toogleIcon } = this.state;
-    return (
-      <div className={styles.header}>
-        <div className={styles.container}>
-          <div className={styles.logotype}>
-            <img className={styles.logoImg} src={logo} alt="LOGO" />
-            <h1 className={styles.logoText}>
-              Slim<span className={styles.logoTextSpan}>Mom</span>
-            </h1>
-          </div>
-          {/* If user is logged - show the burger button (isLogged && button) */}
-          {isLogged && (
-            <div className={styles.usernamebox}>
-              <p>Username</p>
-              <p>|</p>
-              <p className={styles.logoutText}>Выйти</p>
+class Header extends Component {
+
+    static propTypes = {
+        token: PropTypes.string.isRequired,
+    }
+    state = {
+        openModal: false,
+        isLogged: false,
+        toogleIcon: false,
+
+
+    };
+
+
+    toogleModal = e => {
+        this.setState(state => ({ openModal: !state.openModal, toogleIcon: !state.toogleIcon }));
+    };
+    // Here I have to write a function than will check if the User is LoggedIn or not, and change the state apropriately
+
+    componentDidMount() {
+        const { token } = this.props
+        if (!!token) { this.setState({ isLogged: true }) }
+        localStorage.setItem("userToken", "1234")
+    }
+    logOut = () => {
+        localStorage.removeItem("userToken");
+        this.setState(state => ({ isLogged: !state.isLogged }))
+    }
+
+    render() {
+        const { toogleModal, logOut } = this;
+        const { openModal, isLogged, toogleIcon } = this.state;
+        const { token } = this.props
+
+        console.log('token: ', token)
+        return (
+            <div className={styles.header}>
+                <div className={styles.container}>
+                    <div className={styles.logotype}>
+                        <img className={styles.logoImg} src={logo} alt="LOGO" />
+                        <h1 className={styles.logoText}>
+                            Slim<span className={styles.logoTextSpan}>Mom</span>
+                        </h1>
+                    </div>
+                    {/* If user is logged - show the burger button (isLogged && button) */}
+                    {isLogged && (
+                        <div className={styles.usernamebox}>
+                            <p>Username</p>
+                            <p>|</p>
+                            <p className={styles.logoutText}>Выйти</p>
+                        </div>
+                    )}
+                    {isLogged && !toogleIcon && (
+                        // <button className={styles.burgerBtn} onClick={toogleModal}>
+                        <img onClick={toogleModal} className={styles.burger} src={burger} alt="burger button" />
+                        // </button>
+                    )}
+                    {isLogged && toogleIcon && (
+                        // <button className={styles.burgerBtn} onClick={toogleModal}>
+                        <img onClick={toogleModal} className={styles.cross} src={cross} alt="burger button" />
+                        // </button>
+                    )}
+                    {openModal && this.props.windowWidth < 1024 && < Modal />}
+                    {!isLogged && <UserBar />}
+                </div>
+                <div className={styles.grayLine} />
+                {isLogged && (
+                    <div className={styles.greyZone}>
+                        <p className={styles.username}>Username</p> <img onClick={logOut} className={styles.logoutButton} src={logout} />
+                    </div>
+                )}
             </div>
-          )}
-          {isLogged && !toogleIcon && (
-            // <button className={styles.burgerBtn} onClick={toogleModal}>
-            <img onClick={toogleModal} className={styles.burger} src={burger} alt="burger button" />
-            // </button>
-          )}
-          {isLogged && toogleIcon && (
-            // <button className={styles.burgerBtn} onClick={toogleModal}>
-            <img onClick={toogleModal} className={styles.cross} src={cross} alt="burger button" />
-            // </button>
-          )}
-          {openModal && <Modal />}
-          {!isLogged && <UserBar />}
-        </div>
-        <div className={styles.grayLine} />
-        {isLogged && (
-          <div className={styles.greyZone}>
-            <p className={styles.username}>Username</p> <img className={styles.logoutButton} src={logout} />
-          </div>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
+const mapStateToProps = (state) => ({
+
+})
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(windowSize(Header));
--- src/redux/store.js
+++ src/redux/store.js
@@ -22,3 +22,32 @@ const configureStore = () => {
 };
 
 export default configureStore;
+
+
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+
+// export class store extends Component {
+//   static propTypes = {
+//     prop: PropTypes
+//   }
+
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+
+// })
+
+// const mapDispatchToProps = {
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(store)
