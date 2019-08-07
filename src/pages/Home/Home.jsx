da269e09dac813ae2375f2623cb1cf9ebcbb87c0
katehrybkova <katiushka152701@gmail.com>
2019-08-07 11:12:57 +0300

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import style from './Home.module.css';
+// import CalcForm from '../../components/CalcForm/CalcForm';
+// import Result from '../../components/Result/Result';
+// import Header from '../../components/Header/Header';
 
 class Home extends Component {
-  state = {};
-
+  state = {
+    isOpenModal: false
+  };
+  handleToggleModal = () => {
+    this.setState(state => ({
+      isOpenModal: !state.isOpenModal
+    }));
+  };
   render() {
-    return <p>Home</p>;
+    const { isOpenModal } = this.state;
+    return (
+      <div className={style.homeWrapper}>
+        {/* <Header/>
+        <CalcForm />
+        {isOpenModal && <Result onClose={this.handleToggleModal} />} */}
+        <button onClick={this.handleToggleModal} type="button" className={style.calc}>
+          Похудеть
+        </button>
+      </div>
+    );
   }
 }
 
