b1afd884b634768b83a09ec1285007b6f302896d
YarotskiY <abrawa@gmail.com>
2019-08-08 12:36:46 +0300

    addet routs for calc and trying to merge

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import styles from './Dashboard.module.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import windowSize from 'react-window-size';
 
 class Dashboard extends Component {
   state = {
-    isCalcOpen: true
   };
 
-  toggleIsCalcOpen = () => {
-    this.setState(state=>({
-      isCalcOpen: !state.isCalcOpen
-    }))
-  }
-
-
   render() {
-    const { isCalcOpen } = this.state;
-    return <>
+    return (
+      <Router>
+        <>
             <section className={styles.grid}>
              <div className={styles.headerBlock_container}>
               Header
                </div>
              <div className={styles.calcDairyBlock_container}>
-                Calc/DairyBlock
+                <Route path="/calc/" exact component={calc} />
+                <Route path="/diary/" component={diary} />
                </div>
              <div className={styles.summaryBlock_container}>
               SummaryBlock
                </div>
            </section>
-          </>;
+          </>
+      </Router>
+      );
   }
 }
 
