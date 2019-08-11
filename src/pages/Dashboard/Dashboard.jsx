79f165cfb9688b9ad3c1cb5e54228578a6f1a0e1
Yarotskiy Sergey <abrawa@gmail.com>
2019-08-11 18:17:14 +0300

    Resave dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 import styles from './Dashboard.module.css';
 import Header from '../../components/Header/Header';
-import CalcForm from "../../components/CalcForm/CalcForm";
+import CalcForm from '../../components/CalcForm/CalcForm';
 import Summary from '../../components/Summary/Summary';
 import DiaryBlock from '../../components/DiaryBlock/DiaryBlock';
 import windowSize from 'react-window-size';
@@ -13,20 +13,20 @@ export default class Dashboard extends Component {
   render() {
     const { token } = this.props;
     return (
-        <>
-          <section className={styles.grid}>
-            <div className={styles.headerBlock_container}>
-              <Header token={token} />
-            </div>
-            <div className={styles.calcDairyBlock_container}>
-              <Route path="/dashboard" exact component={CalcForm} />
-              <Route path="/dashboard/" component={DiaryBlock} />
-            </div>
-            <div className={styles.summaryBlock_container}>
-              <Summary />
-            </div>
-          </section>
-        </>
+      <>
+        <section className={styles.grid}>
+          <div className={styles.headerBlock_container}>
+            <Header token={token} />
+          </div>
+          <div className={styles.calcDairyBlock_container}>
+            <Route path="/dashboard" exact component={CalcForm} />
+            <Route path="/dashboard/diary" component={DiaryBlock} />
+          </div>
+          <div className={styles.summaryBlock_container}>
+            <Summary />
+          </div>
+        </section>
+      </>
     );
   }
 }
