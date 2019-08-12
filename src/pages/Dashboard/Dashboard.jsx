a087d6f27020db5dd606b49a87442eefc5844267
Andrew Slobodianiuk <speshmillo@gmail.com>
2019-08-12 23:25:54 +0300

    added layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,48 +1,46 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import styles from './Dashboard.module.css';
-import Header from '../../components/Header/Header';
-import CalcForm from '../../components/CalcForm/CalcForm';
-import Summary from '../../components/Summary/Summary';
-import DiaryBlock from '../../components/DiaryBlock/DiaryBlock';
 import { getProductsByDayAction } from '../../redux/actions/productActions';
 import { connect } from 'react-redux';
+import windowSize from 'react-window-size';
 
 class Dashboard extends Component {
   state = {};
   componentDidMount = () => {
-    const token = localStorage.getItem('userToken')
-    const date = new Date().getTime()
-    setProductByDay(token, date);
-  }
-  
+    const token = localStorage.getItem('userToken');
+    const date = new Date().getTime();
+    // setProductByDay(token, date);
+  };
+
   render() {
-    const { token } = this.props;
+    const { windowWidth, location } = this.props;
+
     return (
-      <>
-        <section className={styles.grid}>
-          <div className={styles.headerBlock_container}>
-            <Header token={token} />
-          </div>
-          <div className={styles.calcDairyBlock_container}>
-            <Route path="/dashboard" exact component={CalcForm} />
-            <Route path="/dashboard/diary" component={DiaryBlock} />
-          </div>
-          <div className={styles.summaryBlock_container}>
-            <Summary />
-          </div>
-        </section>
-      </>
+      <section className={styles.grid}>
+        <div className={styles.headerBlock_container}>Header</div>
+
+        <div className={styles.calcDairyBlock_container}>
+          <Route path="/dashboard" exact render={() => <div> Dashbard </div>} />
+          <Route path="/dashboard/diary" render={() => <div> Dairy </div>} />
+        </div>
+
+        {location.pathname === '/dashboard/diary' ? (
+          windowWidth > 767 && <div className={styles.summaryBlock_container}>Summary</div>
+        ) : (
+          <div className={styles.summaryBlock_container}>Summary</div>
+        )}
+      </section>
     );
   }
 }
 
 const mapDispatchToProps = dis => ({
-  setProductsByDay(tok,date) {
-    dis(getProductsByDayAction(tok,date));
+  setProductsByDay(tok, date) {
+    dis(getProductsByDayAction(tok, date));
   }
 });
 export default connect(
   null,
   mapDispatchToProps
-)(Dashboard);
+)(windowSize(Dashboard));
