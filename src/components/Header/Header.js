64ad1de5b2c18f5d18edd4013e88a6e5127a09fd
StellVan <balan.marian2000@gmail.com>
2019-08-15 16:27:35 +0300

    fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
 
   toogleModal = e => {
     this.setState(state => ({ openModal: !state.openModal }));
-  };h
+  };
 
   componentDidMount() {
     const { token } = this.props;
