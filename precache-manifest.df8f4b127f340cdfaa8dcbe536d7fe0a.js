dbd48607f826c33e56c51f95b40610ae4a03a439
Sinas66 <0638175303@ukr.net>
2019-08-21 17:49:27 +0300

    Updates

---
+++ precache-manifest.df8f4b127f340cdfaa8dcbe536d7fe0a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afecc82e7ba0d20c70795890473b05cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0ad477571e4abdca14b",
+    "url": "./static/css/2.bffbeb91.chunk.css"
+  },
+  {
+    "revision": "9ee20949e638f514f223",
+    "url": "./static/css/main.204ac1e6.chunk.css"
+  },
+  {
+    "revision": "a0ad477571e4abdca14b",
+    "url": "./static/js/2.ca93471a.chunk.js"
+  },
+  {
+    "revision": "9ee20949e638f514f223",
+    "url": "./static/js/main.c878b610.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "32aec5ff768da4ae771c95fac7593191",
+    "url": "./static/media/GothamProBlack.32aec5ff.woff"
+  },
+  {
+    "revision": "37c37df55859d300a0bc06ebab70beda",
+    "url": "./static/media/GothamProBlack.37c37df5.ttf"
+  },
+  {
+    "revision": "6a9b59904c2c5341235ddba28870a854",
+    "url": "./static/media/GothamProBlack.6a9b5990.eot"
+  },
+  {
+    "revision": "35b1157a9fb02decfee874860c0e9573",
+    "url": "./static/media/GothamProBold.35b1157a.eot"
+  },
+  {
+    "revision": "45aa54635ee87503742d3b4af08a808f",
+    "url": "./static/media/GothamProBold.45aa5463.ttf"
+  },
+  {
+    "revision": "f5cfc42e97cd9c8c75a24f14c2349aa5",
+    "url": "./static/media/GothamProBold.f5cfc42e.woff"
+  },
+  {
+    "revision": "00565e2c147187e7198633d8a44628cd",
+    "url": "./static/media/GothamProLight.00565e2c.eot"
+  },
+  {
+    "revision": "3c6c14f0c938b91ca568cb59f6febdc5",
+    "url": "./static/media/GothamProLight.3c6c14f0.woff"
+  },
+  {
+    "revision": "792e5785377b31db37697448eba44281",
+    "url": "./static/media/GothamProLight.792e5785.ttf"
+  },
+  {
+    "revision": "96481502992271cdbbc95b36f6de1627",
+    "url": "./static/media/add-24px.96481502.svg"
+  },
+  {
+    "revision": "3be5736bbe17e7bed7f15dec0d96a1d2",
+    "url": "./static/media/arrow_back.3be5736b.svg"
+  },
+  {
+    "revision": "5633ea060d631c4b8ea95ef4d0f4ce96",
+    "url": "./static/media/arrow_down-24px.5633ea06.svg"
+  },
+  {
+    "revision": "736177526887a4ee0986824961524317",
+    "url": "./static/media/clear-24px.73617752.svg"
+  },
+  {
+    "revision": "db3fe99ebfa2371ead68a46b0560fc54",
+    "url": "./static/media/date_range_black_24px.db3fe99e.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "./static/media/delete.777b93c6.svg"
+  },
+  {
+    "revision": "4cc905c1d1756a079dee3253804a31c7",
+    "url": "./static/media/desktop-bg-REGISTER.4cc905c1.jpg"
+  },
+  {
+    "revision": "bcf4f1bb76c7df511e43bfda8f8f96a2",
+    "url": "./static/media/greenleaves-tablet.bcf4f1bb.png"
+  },
+  {
+    "revision": "a6c33e9b6724cb2e13fafd0b246db2d7",
+    "url": "./static/media/greenleaves.a6c33e9b.png"
+  },
+  {
+    "revision": "9008e6cc0866993e4f61b719a13bbe9a",
+    "url": "./static/media/home_descktop.9008e6cc.png"
+  },
+  {
+    "revision": "24167c21e5a57cbeec7bfb61be8e3dd1",
+    "url": "./static/media/home_tablet.24167c21.png"
+  },
+  {
+    "revision": "5186b22855580daa1b3c4a0b58ced97a",
+    "url": "./static/media/logo-png.5186b228.png"
+  },
+  {
+    "revision": "4bcd805ab3e69025d36d536b0eee04e7",
+    "url": "./static/media/logout.4bcd805a.svg"
+  },
+  {
+    "revision": "4e796dac6b6f8fc77ed38a7212426eb8",
+    "url": "./static/media/menu-24px.4e796dac.svg"
+  },
+  {
+    "revision": "f8e95c0ccb17b2e14025efef49376e9e",
+    "url": "./static/media/russia.f8e95c0c.svg"
+  },
+  {
+    "revision": "b12335dc89b646225baf55947fc88cee",
+    "url": "./static/media/tablet-bg-register.b12335dc.jpg"
+  },
+  {
+    "revision": "2427465f9fc2f94e2e6417dd43cd99e5",
+    "url": "./static/media/ukraine.2427465f.svg"
+  }
+]);
